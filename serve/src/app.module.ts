b6a123c3fe3bd2386d575938bd64e9534e5f6252
Muchamad Faizal Tri Prasetyo <59045627+fazemii01@users.noreply.github.com>
2025-07-26 17:22:43 +0700

    fix 404 not found

--- serve/src/app.module.ts
+++ serve/src/app.module.ts
@@ -21,7 +21,7 @@ import { ChatbotModule } from './chatbot/chatbot.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        //type: 'postgres',
+        type: 'postgres',
         //host: configService.get<string>('DB_HOST'),
         //port: configService.get<number>('DB_PORT', 5432),
         //username: configService.get<string>('DB_USERNAME'),
