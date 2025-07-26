75491c1a8def651bca4a6d12f9eb44cec37349bf
Muchamad Faizal Tri Prasetyo <59045627+fazemii01@users.noreply.github.com>
2025-07-26 16:50:51 +0700

    fix 404 not found

--- serve/src/app.module.ts
+++ serve/src/app.module.ts
@@ -21,12 +21,13 @@ import { ChatbotModule } from './chatbot/chatbot.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT', 5432),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
+        //type: 'postgres',
+        //host: configService.get<string>('DB_HOST'),
+        //port: configService.get<number>('DB_PORT', 5432),
+        //username: configService.get<string>('DB_USERNAME'),
+        //password: configService.get<string>('DB_PASSWORD'),
+        //database: configService.get<string>('DB_DATABASE'),
+        url: configService.get<string>('DATABASE_URL'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true, // Shouldn't be used in production
         ssl: {
