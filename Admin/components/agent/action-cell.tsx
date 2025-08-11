ced7ab1f708afc33beccfa86952da05610daec66
marcellyuliansyah <marcellyuliansyah57599@gmail.com>
2025-08-11 22:41:20 +0700

    push message dialog

---
+++ Admin/components/agent/action-cell.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { Agent } from "@/types/agent";
+import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal } from "lucide-react";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
+import ConfirmMessage from "@/components/confirm-message";
+const ActionAgentCell = ({ agent }: { agent: Agent }) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0 cursor-pointer">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem className="cursor-pointer">
+            <IconEdit />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => setOpen(true)}
+          >
+            <IconTrash />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <ConfirmMessage open={open} setOpen={setOpen} />
+    </>
+  );
+};
+
+export default ActionAgentCell;
