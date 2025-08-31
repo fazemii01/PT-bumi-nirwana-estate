19d108c324f817d319295adbeaf044629da74e92
Muchamad Faizal Tri Prasetyo <59045627+fazemii01@users.noreply.github.com>
2025-08-31 23:24:55 +0700

    last try

--- serve/api/index.ts
+++ serve/api/index.ts
@@ -4,24 +4,42 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-const server = express();
+// const server = express();
 
-export const bootstrap = async () => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
-  );
+// export const bootstrap = async () => {
+//   const app = await NestFactory.create(
+//     AppModule,
+//     new ExpressAdapter(server),
+//   );
 
-  app.use(cookieParser());
-  app.enableCors({
-    origin: true, 
-    credentials: true,
-  });
-  app.useGlobalPipes(new ValidationPipe());
+//   app.use(cookieParser());
+//   app.enableCors({
+//     origin: true, 
+//     credentials: true,
+//   });
+//   app.useGlobalPipes(new ValidationPipe());
 
 
-  await app.init();
-};
-bootstrap();
-export default server;
+//   await app.init();
+// };
+// bootstrap();
+// export default server;
+
+let cachedApp;
+
+async function bootstrap() {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.init();
+    cachedApp = app.getHttpAdapter().getInstance();
+  }
+  return cachedApp;
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const server = await bootstrap();
+  server(req, res);
+}
