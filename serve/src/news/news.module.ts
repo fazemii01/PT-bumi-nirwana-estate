6a12a13b5d2fc6138e5b4c30a272162acf4b060f
ifan0507 <ipan.lmj0507@gmail.com>
2025-09-25 21:26:06 +0700

    pull create building

--- serve/src/news/news.module.ts
+++ serve/src/news/news.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-// import { NewsService } from './news.service';
-// import { NewsController } from './news.controller';
+import { NewsService } from './news.service';
+import { NewsController } from './news.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { News } from '@/news/entities/news.entity';
 import { NewsCategory } from '@/news_category/entities/news_category.entity';
 import { Property } from '@/properties/entities/property.entity';
 import { NewsImages } from '@/news/entities/news_images.entity';
 import { DeviceToken } from '@/device-token/entities/device-token.entity';
-// import { FcmModule } from '@/fcm/fcm.module';
+import { FcmModule } from '@/fcm/fcm.module';
 
 @Module({
   imports: [
@@ -20,7 +20,7 @@ import { DeviceToken } from '@/device-token/entities/device-token.entity';
     ]),
     FcmModule,
   ],
-  // controllers: [NewsController],
-  // providers: [NewsService],
+  controllers: [NewsController],
+  providers: [NewsService],
 })
 export class NewsModule {}
